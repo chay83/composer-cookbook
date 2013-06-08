afcea9e789a032428e1cc772220aba162949d55f
Chay Palmer <chay@chay.com.au>
2013-06-09 00:27:53 +1000

    make it work

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,22 +2,36 @@
 # Cookbook Name:: composer
 # Recipe:: default
 #
-# Copyright 2012, Media & Software Studio
-#
-# All rights reserved - Do Not Redistribute
+# Copyright 2012-2013, Escape Studios
 #
 
-execute "get-composer" do
-  cwd node[:composer][:base_path]
-  command "sudo wget http://getcomposer.org/composer.phar"
-  creates "composer.phar"
-  creates "#{node[:composer][:base_path]}/composer.phar"
-  action :run
+include_recipe "php"
+
+#install/upgrade curl
+package "curl" do
+	action :upgrade
 end
 
-execute "install-composer" do
-  cwd node[:composer][:base_path]
-  command "sudo php composer.phar install --prefer-source"
-  creates "#{node[:composer][:base_path]}/composer.lock"
-  action :run
+command = "curl -s https://getcomposer.org/installer | php"
+
+unless node[:composer][:install_globally]
+	unless node[:composer][:install_dir].nil? || node[:composer][:install_dir].empty?
+		command = command + " -- --install-dir=#{node[:composer][:install_dir]}"
+    end
+end
+
+bash "download_composer" do
+	cwd "#{Chef::Config[:file_cache_path]}"
+	code <<-EOH
+		#{command}
+	EOH
+end
+
+if node[:composer][:install_globally]
+	bash "move_composer" do
+		cwd "#{Chef::Config[:file_cache_path]}"
+		code <<-EOH
+			sudo mv composer.phar #{node[:composer][:prefix]}/bin/composer
+		EOH
+	end
 end
